af33701fcb2e62eb6666a19efe477925aea0f4e7
Ashish Trehan <ashishtrehan002@gmail.com>
2023-03-03 23:45:35 +0530

    .

--- disease_predictor/settings.py
+++ disease_predictor/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iup^+zs%sj!=0p@k2qp-nus3ujz!8r3r0rwa)-8cu#cs^200m3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
